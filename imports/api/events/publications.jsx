22ac63ac72a8fb80b3824b1ffa5d871991cc64b1
Mateusz Stanuch <mateusz.stanuch@gmail.com>
2016-07-09 12:18:45 +0200

    Update: comment event-list

--- imports/api/events/publications.jsx
+++ imports/api/events/publications.jsx
@@ -1,14 +1,14 @@
 import { Meteor } from 'meteor/meteor';
 import EventList from './events';
 
-Meteor.publish('events-list', function () {
+/*Meteor.publish('events-list', function () {
     return EventList.find({}, { fields: {
         name: 1,
         owner: 1,
         date: 1,
         place: 1,
     }});
-});
+});*/
 
 Meteor.publish('single-event', function (id) {
     const currentEvent = id;

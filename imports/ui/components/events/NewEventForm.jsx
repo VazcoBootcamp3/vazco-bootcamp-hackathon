87d332d39a72e2fcc3a1b7164d894b0e11a50270
Mateusz Tuński <mateusz@tunski.co>
2016-07-09 14:15:58 +0200

    New event form is using materialize

--- imports/ui/components/events/NewEventForm.jsx
+++ imports/ui/components/events/NewEventForm.jsx
@@ -7,25 +7,66 @@ export default class NewEventForm extends Component {
     name: '',
     organizer: '',
     date: '',
-    place: '',
+    location: '',
     description: '',
   }
 
   handleSubmit = (ev) => {
     ev.preventDefault()
-
     this.props.router.push('/admin/events/eventId')
   }
 
-  // handleInputChange = (ev) => {
-  //   this.setState()
-  // }
+  componentDidMount() {
+    const input = $(this.refs.datepicker).pickadate()
+    const picker = $(input).pickadate('picker')
+
+    picker.on({
+      set: ev => { this.setState({ date: ev.select }) },
+    })
+  }
+
+  handleInputChange = (ev) => {
+    ev.preventDefault()
+    this.setState({ [ev.target.id]: ev.target.value })
+  }
 
   render() {
     return (
-      <div>
-        <form onSubmit={this.handleSubmit}>
-          <button>Submit</button>
+      <div className="row">
+        <form onSubmit={this.handleSubmit} className="col s12">
+          <div className="row">
+            <div className="input-field col s6">
+              <label htmlFor="name">Event name</label>
+              <input id="name" type="text" value={this.state.name} onChange={this.handleInputChange} />
+            </div>
+          </div>
+          <div className="row">
+            <div className="input-field col s6">
+              <label htmlFor="organizer">Event creator</label>
+              <input id='organizer' type="text" value={this.state.organizer} onChange={this.handleInputChange} />
+            </div>
+          </div>
+          <div className="row">
+            <div className="input-field col s6">
+              <label htmlFor="date">Event date</label>
+              <input id="date" type="date" className="datepicker" ref="datepicker" />
+            </div>
+          </div>
+          <div className="row">
+            <div className="input-field col s6">
+              <label htmlFor="location">Event location</label>
+              <input id="location" type="text" value={this.state.location} onChange={this.handleInputChange} />
+            </div>
+          </div>
+          <div className="row">
+            <div className="input-field col s6">
+              <label htmlFor="description">Event description</label>
+              <textarea id="description" className="materialize-textarea" value={this.state.description} onChange={this.handleInputChange} />
+            </div>
+          </div>
+          <div className="row">
+            <button className="waves-effect waves-light btn">Submit</button>
+          </div>
         </form>
       </div>
     )

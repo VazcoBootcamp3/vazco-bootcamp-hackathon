5e11c6364aa5afcad4fc96226e76c8d077ac3b63
Mateusz Stanuch <mateusz.stanuch@gmail.com>
2016-07-09 13:35:37 +0200

    Remove unused publication

--- imports/api/events/publications.jsx
+++ imports/api/events/publications.jsx
@@ -1,15 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import EventList from './events';
 
-/*Meteor.publish('events-list', function () {
-    return EventList.find({}, { fields: {
-        name: 1,
-        owner: 1,
-        date: 1,
-        place: 1,
-    }});
-});*/
-
 Meteor.publish('single-event', function (id) {
     const currentEvent = id;
     return EventList.find({_id: currentEvent});
